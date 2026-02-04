3ed36387d6dd2ed71faf9f13bd98ca80d3cd4d11
Ladislav Jaromír Pleštil <ladaplestil09@seznam.cz>
2026-02-04 13:03:03 +0100

    Added PWA functionality

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'jolanda-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Playfair+Display:ital,wght@0,400;0,700;1,400&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
