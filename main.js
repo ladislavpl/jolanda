fe254a488035448c8e15b4fed72a47c82c4458a7
Ladislav Jaromír Pleštil <ladaplestil09@seznam.cz>
2026-02-05 09:51:30 +0100

    updated electron app

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        autoHideMenuBar: true,
+        icon: path.join(__dirname, 'assets/icons/icon.png'),
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true
